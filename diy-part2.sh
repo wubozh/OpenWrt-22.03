ef993a4957ce704d573e8d1ae13c90aa542af800
wubozh <38520396+wubozh@users.noreply.github.com>
2023-06-05 20:30:01 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,10 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+
+rm -rf ./feeds/packages/net/adguardhome && git clone https://github.com/kongfl888/luci-app-adguardhome.git ./package/luci-app-adguardhome
+wget https://github.com/vernesong/OpenClash/archive/refs/tags/v0.45.121-beta.tar.gz && tar xvf v0.45.121-beta.tar.gz -C ./package
+git clone https://github.com/messense/aliyundrive-webdav.git ./package/aliyundive-webdav
+git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
+git clone https://gitee.com/wubozh/feeds-kiddin9.git /package/kiddin9
+git clone https://github.com/kenzok8/small.git package/small
